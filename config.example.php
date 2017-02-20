3e1196e6971b03a062507169f84fd0b22176046c
Sam Reed <reedy@wikimedia.org>
2017-02-20 01:04:47 +0000

    Add basic config.example.php
    Use config.php in upwork-time-logger.php

---
+++ config.example.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Apply for an API key at https://www.upwork.com/services/api/apply
+ *
+ * Requires "Modify your workdiary" permission.
+ */
+
+$consumerKey = '';
+$consumerSecret = '';
--- upwork-time-logger.php
+++ upwork-time-logger.php
@@ -1,15 +1,21 @@
 <?php
 
-if ( is_readable( "$IP/vendor/autoload.php" ) ) {
-	require_once "$IP/vendor/autoload.php";
+if ( is_readable( __DIR__ . 'vendor/autoload.php' ) ) {
+	require_once __DIR__ . '/vendor/autoload.php';
+}
+
+if ( is_readable( __DIR__ . '/config.php' ) {
+	require_once __DIR__ . '/config.php';
+} else {
+	die( 'Please copy config.example.php to config.php and change the variables values with OAuth credentials from Upwork.' );
 }
 
 $config = new \Upwork\API\Config(
-    [
-        'consumerKey'       => 'xxxxxxxxxxxxxxxxxxxxxxxxxxxx',
-        'consumerSecret'    => 'xxxxxxxxxxxxxx',
-        'debug'             => true,
-    ]
+	[
+		'consumerKey'       => $consumerKey,
+		'consumerSecret'    => $consumerSecret,
+		'debug'             => true,
+	]
 );
 
 $client = new \Upwork\API\Client( $config );
